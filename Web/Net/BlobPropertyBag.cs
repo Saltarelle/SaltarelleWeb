8d5ace331a2ed8fef8d81e7eff38d48fe30b9df5
Timothy Fries <drysart@gmail.com>
2013-04-04 03:39:07 -0500

    Added missing XMLHttpRequest members and Blob

---
+++ Web/Net/BlobPropertyBag.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace System.Net
+{
+    [IgnoreNamespace, Imported]
+    public class BlobPropertyBag
+    {
+        [InlineCode("{}")]
+        public BlobPropertyBag()
+        {
+        }
+
+        [InlineCode("{'type':{type}}")]
+        public BlobPropertyBag(string type)
+        {
+        }
+
+        [IntrinsicProperty]
+        public string Type { get; set; }
+    }
+}
