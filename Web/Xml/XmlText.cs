4127834f6085314644a812c8a13488cf0d55adf1
Erik Källén <erik_kallen@hotmail.com>
2014-02-22 14:04:11 +0100

    Removed members Name and Value from XmlNode to avoid collisions (#12)

---
+++ Web/Xml/XmlText.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace System.Xml {
+	public partial class XmlText {
+		[IntrinsicProperty]
+		[ScriptName("nodeName")]
+		public string Name {
+			get { return null; }
+			set { }
+		}
+
+		[IntrinsicProperty]
+		[ScriptName("nodeValue")]
+		public string Value {
+			get { return null; }
+			set { }
+		}
+	}
+}
