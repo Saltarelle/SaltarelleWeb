8d0017fd031473ab7df1b883d1b08a9e471bcc2a
n9 <n9>
2014-12-05 23:30:09 +0100

    Fix navigator.getUserMedia webkit(TypeError: Illegal invocation) + moz(typo)

--- Web/Html/Navigator.cs
+++ Web/Html/Navigator.cs
@@ -3,10 +3,10 @@ using System.Runtime.CompilerServices;
 
 namespace System.Html {
 	public partial class Navigator {
-		[InlineCode("(navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozUserMedia || navigator.msGetUserMedia)({params}, {onsuccess})")]
+		[InlineCode("(navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia).call(navigator, {params}, {onsuccess})")]
 		public static void GetUserMedia(MediaStreamOptions @params, Action<LocalMediaStream> onsuccess) {}
 
-		[InlineCode("(navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozUserMedia || navigator.msGetUserMedia)({params}, {onsuccess}, {onerror})")]
+		[InlineCode("(navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia).call(navigator, {params}, {onsuccess}, {onerror})")]
 		public static void GetUserMedia(MediaStreamOptions @params, Action<LocalMediaStream> onsuccess, Action<string> onerror) {}
 	}
 }
