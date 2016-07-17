3cfb36b4e8f756712ab45678e4456bdb8a2edb70
LeonidVeriga <leonid@object.net>
2016-07-17 17:50:38 +0300

    Sort enum members by Name or Value (name "None" is lowest)

---
+++ Generator/Generator/EnumMemberDeclarationComparer.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using ICSharpCode.NRefactory.CSharp;
+
+namespace Generator
+{
+    internal class EnumMemberDeclarationComparer : IComparer<EnumMemberDeclaration>
+    {
+        public bool ByValue { get; set; }
+
+        public EnumMemberDeclarationComparer(bool byValue)
+        {
+            this.ByValue = byValue;
+        }
+
+        public int Compare(EnumMemberDeclaration a, EnumMemberDeclaration b)
+        {
+            if (ByValue)
+            {
+                var xa = a.Initializer as PrimitiveExpression;
+                var xb = b.Initializer as PrimitiveExpression;
+
+                var comparer = Comparer<object>.Default;
+
+                if (xa == null || xb == null)
+                {
+                    return comparer.Compare(xa, xb);
+                }
+
+                return comparer.Compare(xa.Value, xb.Value);
+            }
+
+            var lowest = "None";
+            var ba = a.Name == lowest;
+            var bb = b.Name == lowest;
+
+            if (ba == bb)
+            {
+                return string.Compare(a.Name, b.Name);
+            }
+
+            if (ba)
+            {
+                return -1;
+            }
+
+            return 1;
+        }
+    }
+}
